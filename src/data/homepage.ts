d62c2b07696ee492f10a4a1cf78dc1a928270dc8
Joe Abell <jsphabll@gmail.com>
2024-10-31 21:34:25 +0000

    sync

--- src/data/homepage.ts
+++ src/data/homepage.ts
@@ -1,6 +1,7 @@
 const homepageData = {
     title: "Bookbinding Tutorials",
-    description: "Whether you are a beginner or an experienced bookbinder, this site offers tutorials, resources, and pdfs ready to print, to enhance your skills and create beautiful, handcrafted books."
+    description: "Whether you are a beginner or an experienced bookbinder, this site offers tutorials, resources, and pdfs ready to print, to enhance your skills and create beautiful, handcrafted books.",
+    metaDescription: "Whether you are a beginner or an experienced bookbinder, this site offers tutorials, resources, and pdfs ready to print, to enhance your skills and create beautiful, handcrafted books."
 };
 
 export default homepageData;
